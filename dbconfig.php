18da29b5adb5fb6149ec282889ee5926d0126513
Christian Talon <criscore98@gmail.com>
2020-10-06 19:49:01 +0800

    Force update

--- dbconfig.php
+++ dbconfig.php
@@ -15,6 +15,7 @@ $password = getenv("DB_PASSWORD"); // Mysql password
 $db_name = getenv("DB_NAME"); // Database name
 $db_port = getenv("DB_PORT"); //port
 // echo $host;
+
 // Create connection
 $conn = new mysqli($host, $username, $password, $db_name, $db_port);
 
