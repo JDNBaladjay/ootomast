4b407658da1f89d54664a21b34159d4fabf9250c
JDNBaladjay <smoltortle@gmail.com>
2020-10-06 19:00:15 +0800

    added port

--- dbconfig.php
+++ dbconfig.php
@@ -13,7 +13,7 @@ $host = getenv("DB_HOST"); // Host name
 $username = getenv("DB_USERNAME"); // Mysql username
 $password = getenv("DB_PASSWORD"); // Mysql password
 $db_name = getenv("DB_NAME"); // Database name
-
+$db_port = getenv("DB_PORT"); //port
 echo $host;
 // Create connection
-$conn = mysqli_connect($host, $username, $password, $db_name);
+$conn = mysqli_connect($host, $username, $password, $db_name, $db_port);
